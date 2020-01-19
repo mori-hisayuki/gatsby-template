e54456f0d13f1d5685f2cb0955f95c21476c8048
hisayuki <siruvalice@gmail.com>
2020-01-19 22:17:56 +0900

    create_projectの完成

--- docker/project_init/create_project.sh
+++ docker/project_init/create_project.sh
@@ -2,8 +2,7 @@ docker-compose run --rm develop sh -c "
 yarn global add gatsby-cli && \
 gatsby new gatsby-app && \
 cp ./docker/project_init/init_file/* gatsby-app && \
-mv gatsby-app/* ./ && \
-mv gatsby-app/.* ./ && \
+mv gatsby-app/* gatsby-app/.prettier* . && \
 rm -f yarn.lock && \
-yarn && yarn cache clean && \
+yarn && \
 rm -rf gatsby-app"
