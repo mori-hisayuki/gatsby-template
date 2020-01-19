43458a9eadc04fc256e86a8cf3b5b03fb33f6b9d
hisayuki <siruvalice@gmail.com>
2020-01-19 16:23:42 +0900

    node_moduleを一旦削除してから再度インストール

--- docker/project_init/create_project.sh
+++ docker/project_init/create_project.sh
@@ -2,5 +2,7 @@ docker-compose run --rm app sh -c "yarn global add gatsby-cli && \
 gatsby new gatsby-app && \
 mv ./docker/project_init/init_file/* gatsby-app && \
 mv gatsby-app/* ./ && \
-yarn install && \
-rm -rf gatsby-app"
+rm -rf node_modules && \
+rm -rf gatsby-app && \
+yarn && yarn cache clean"
+
