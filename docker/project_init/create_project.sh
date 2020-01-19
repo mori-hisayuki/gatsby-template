3fdb1a4df42b6b3e3eaee1751bc3cf67e8ebbac6
hisayuki <siruvalice@gmail.com>
2020-01-19 20:05:07 +0900

    dockerの修正

--- docker/project_init/create_project.sh
+++ docker/project_init/create_project.sh
@@ -1,8 +1,7 @@
-docker-compose run --rm app sh -c "yarn global add gatsby-cli && \
-gatsby new gatsby-app && \
+docker-compose run --rm app sh -c "
 mv ./docker/project_init/init_file/* gatsby-app && \
 mv gatsby-app/* ./ && \
-rm -rf node_modules && \
+rm -rf node_modules/sharp && \
 rm -rf gatsby-app && \
 yarn && yarn cache clean"
 
